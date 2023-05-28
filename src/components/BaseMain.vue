d8cdb6a9bd78a4e90e6e68850f22e8968b074113
Salvatore De Paola <salvatoredepaola8@gmail.com>
2023-05-28 20:59:18 +0200

    terzo commit

--- src/components/BaseMain.vue
+++ src/components/BaseMain.vue
@@ -29,6 +29,9 @@ export default {
                 this.store.series = result.data.results
             })
         },
+        getiniziali(autore){
+            return autore[0] + autore.split(" ")[1][0];
+        }
     },
     mounted() {
 
@@ -37,109 +40,116 @@ export default {
 </script>
 
 <template>
-
-<main>
-    <div class=" mt-5">
-        <input @keyup.enter="avviaRicercaFilms(),avviaRicercaSeries()" type="text" v-model="store.searchText">
-        <button @click="avviaRicercaFilms(),avviaRicercaSeries()">DEBUG</button>
-        <h2 class="text-light">FILM</h2>
-        <div class="row">
-            <div class="col d-flex flex-wrap">
-                <!-- <div v-for="film in this.store.movies" class="my_card">
-                    <h4>{{ film.title }}</h4>
-                    <h5>{{ film.original_title }}</h5>
-                    <p>{{ film.original_language }}</p>
-                    <p>{{ film.vote_average }}</p>
-                </div> -->
-                <div v-for="movie in this.store.movies" class="flip-box">
-                    <div class="flip-box-inner">
-                        <div class="flip-box-front">
-                            <img :src="`${this.store.imgApiUrl}${movie.backdrop_path}`" alt="Paris">
+    <main>
+        <div class="container-fluid mt-5">
+            <input @keyup.enter="avviaRicercaFilms(), avviaRicercaSeries()" type="text" v-model="store.searchText">
+            <button @click="avviaRicercaFilms(), avviaRicercaSeries()">DEBUG</button>
+            <h2 class="text-light">FILM</h2>
+            <div class="row">
+                <div class="col d-flex flex-wrap">
+                    <!-- <div v-for="film in this.store.movies" class="my_card">
+                        <p>{{ film.title }}</p>
+                        <p>{{ film.original_title }}</p>
+                        <p>{{ film.original_language }}</p>
+                        <p>{{ film.vote_average }}</p>
+                    </div> -->
+                    <div v-for="movie in this.store.movies" class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <img v-if="movie.backdrop_path" :src="`${this.store.imgApiUrl}${movie.backdrop_path}`" alt="Paris">
+                                <div v-else >
+                                    <p></p>
+                                </div>
+                            </div>
+                            <div class="flip-box-back">
+                                <p>Titolo: <small>{{ movie.title }}</small></p>
+                                <p>Titolo originale: <small>{{ movie.original_title }}</small></p>
+                                <p>Lingua: <small>{{ movie.original_language }}</small></p>
+                                <p>Voto: <small>{{ movie.vote_average }}</small></p>
+                            </div>
                         </div>
-                        <div class="flip-box-back">
-                            <h4>Titolo: {{ movie.title }}</h4>
-                            <h5>titolo originale: {{ movie.original_title }}</h5>
-                            <p>Lingua: {{ movie.original_language }}</p>
-                            <p>Voto: {{ movie.vote_average }}</p>
+                    </div>
+                </div>
+            </div>
+            <h2 class="text-light">SERIE</h2>
+            <div class="row">
+                <div class="col d-flex flex-wrap">
+                    <!-- <div v-for="serie in this.store.series" class="my_card">
+                        <p>{{ serie.name }}</p>
+                        <p>{{ serie.original_name }}</p>
+                        <p>{{ serie.original_language }}</p>
+                        <p>{{ serie.vote_average }}</p>
+                    </div> -->
+                    <div v-for="serie in this.store.series" class="flip-box">
+                        <div class="flip-box-inner">
+                            <div class="flip-box-front">
+                                <img v-if="serie.backdrop_path" :src="`${this.store.imgApiUrl}${serie.backdrop_path}`" alt="Paris">
+                                <div v-else>
+
+                                </div>
+                            </div>
+                            <div class="flip-box-back">
+                                <p>Titolo: <small>{{ serie.name }} </small></p>
+                                <p>Titolo originale: <small>{{ serie.original_name }} </small></p>
+                                <p>Lingua: <small>{{ serie.original_language }} </small></p>
+                                <p>Voto: <small>{{ serie.vote_average }} </small></p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <h2 class="text-light">SERIE</h2>
-        <div class="row">
-            <div class="col d-flex flex-wrap">
-                <!-- <div v-for="serie in this.store.series" class="my_card">
-                    <h4>{{ serie.name }}</h4>
-                    <h5>{{ serie.original_name }}</h5>
-                    <p>{{ serie.original_language }}</p>
-                    <p>{{ serie.vote_average }}</p>
-                </div> -->
-                <div v-for="serie in this.store.series" class="flip-box">
-                    <div class="flip-box-inner">
-                        <div class="flip-box-front">
-                            <img :src="`${this.store.imgApiUrl}${serie.backdrop_path}`" alt="Paris">
-                        </div>
-                        <div class="flip-box-back">
-                            <h4>Titolo: {{ serie.name }}</h4>
-                            <h5>titolo originale: {{ serie.original_name }}</h5>
-                            <p>Lingua: {{ serie.original_language }}</p>
-                            <p>Voto: {{ serie.vote_average }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</main>
-
+    </main>
 </template>
 
 <style lang="scss" scoped>
 @use "../styles/partials/mixins" as *;
-.my_card{
-    text-align: center;
-    width: calc(20% - 2rem);
-    margin: 1rem;
-    background-color: red;
+
+p {
+    margin: 0;
+    font-weight: 600;
 }
+small{
+    font-weight: 300;
+}
+
 .flip-box {
-  background-color:transparent;
-  margin: calc(1rem - -3px );
-  width: 342px;
-  height: 193px;
-  perspective: 1000px; 
+    background-color: transparent;
+    margin: calc(1rem);
+    width: 342px;
+    height: 193px;
+    perspective: 1000px;
 }
 
 .flip-box-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 0.8s;
-  transform-style: preserve-3d;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    transition: transform 0.8s;
+    transform-style: preserve-3d;
 }
 
 .flip-box:hover .flip-box-inner {
-  transform: rotateY(180deg);
+    transform: rotateY(180deg);
 }
 
-.flip-box-front, .flip-box-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  -webkit-backface-visibility: hidden; 
-  backface-visibility: hidden;
+.flip-box-front,
+.flip-box-back {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    -webkit-backface-visibility: hidden;
+    backface-visibility: hidden;
 }
 
 .flip-box-front {
-  color: black;
-
+    color: black;
 }
 
 .flip-box-back {
-  background-color: dodgerblue;
-  color: white;
-  transform: rotateY(180deg);
+    border: 1px solid white;
+    background-color: black;
+    color: white;
+    transform: rotateY(180deg);
 }
 </style>
